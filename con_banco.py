67ddfbb4c0e29bea820771d40d2014fdee576444
Anaellen Silva <anaellen.silva123@gmail.com>
2024-04-24 10:59:12 -0300

    third commit

--- con_banco.py
+++ con_banco.py
@@ -1,7 +1,34 @@
 import mysql.connector
+from mysql.connector import errorcode
 
-conn = mysql.connector.connect(user = 'root',
-                               password = 'G-op198Ana',
-                               host = '127.0.0.1',
-                               database = 'jogoteca')
-conn.close()
+config = {
+    'user': 'root',
+    'password': 'G-op198Ana',
+    'host': '127.0.0.1',
+    'database': 'jogoteca',
+    'raise_on_warnings': True
+
+
+}
+
+#conn = mysql.connector.connect(user = 'root',
+ #                              password = 'G-op198Ana',
+#                               host = '127.0.0.1',
+#                               database = 'jogoteca')
+
+
+try:
+    conn = mysql.connector.connect(
+        **config
+    )
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Somenthing is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+
+else:
+    conn.close()
