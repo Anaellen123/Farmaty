2697403ed1160bd0f12ad90b7d5c1820fb67a0b3
Anaellen Silva <anaellen.silva123@gmail.com>
2024-05-12 13:06:23 -0300

    eleventh commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect,url_for
 
 app = Flask(__name__)
 
@@ -7,6 +7,17 @@ app = Flask(__name__)
 def index():
      return render_template("index.html")
 
+
+@app.route('/criar', methods=['POST',])
+def consumo_ml():
+    mg = request.form['mg']
+    ml = request.form['ml']
+    quantidade_mg_dose = request.form['dose']
+    dose_ml = 1
+    calc1 = mg * dose_ml
+    calc2 = quantidade_mg_dose * ml
+    result = calc2 / calc1
+    return "o paciente devera tomar {}ml a cada hora preescrita".format(result), redirect(url_for('index'))
 
 
 app.run(debug=True)
