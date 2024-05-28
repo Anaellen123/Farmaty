4d7ef91aecc5ec9811c435eecf26d2349365aec1
Anaellen Silva <anaellen.silva123@gmail.com>
2024-05-28 12:52:28 -0300

    twenty second commit

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 
 class dosagem:
-     def __init__(self, dose, mg, ml):
+     def __init__(self, dose, mg, ml, hora):
           self.dose = dose
           self.mg = mg
           self.ml = ml
+          self.hora = hora
 
      def consumo_por_ml(self, dose, mg, ml):
           quantidade_mg_dose = dose
@@ -20,6 +21,11 @@ class dosagem:
           calc2 = quantidade_mg_dose * total_ml_da_embalagem
           result = calc2 / calc1
           return result
+
+     def horas_dia(self, hora):
+          vezes = hora
+          divs = 24 / vezes
+          return int(divs)
 
 
 lista = []
@@ -36,14 +42,15 @@ def criar():
      dose = int(request.form['dose'])
      mg = int(request.form['mg'])
      ml = int(request.form['ml'])
-     poso = dosagem(dose, mg, ml)
+     hora = int(request.form['hora'])
+     poso = dosagem(dose, mg, ml, hora)
      lista.append(poso)
-
+     horario = "{} horas pelo tempo preescrito".format(poso.horas_dia(hora))
      result = "Será necessário {} ml a cada ".format(poso.consumo_por_ml(dose,mg,ml))
 
 
 
-     return render_template('index.html', paciente=lista, dose = result)
+     return render_template('index.html', paciente=lista, dose = result, hora = horario)
 
 
 
